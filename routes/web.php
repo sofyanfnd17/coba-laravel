8fccaef56869e8e83f0a9d3d7c5cf238d72ecf04
sofyan efendi <40664922+sofyanfnd17@users.noreply.github.com>
2023-07-07 09:45:28 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => 'home',
+        "active" => 'home'
+    ]);
+});
+
+Route::get('/galleries', function () {
+    return view('galleries', [
+        "title" => "galleries",
+        "active" => "galleries"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "active" => 'about',
+        "name" => "Abaddon",
+        "email" => "Abaddon@gmail.com",
+        "image" => "d2.jpg"
+    ]);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/login', [loginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [loginController::class, 'authenticate']);
+Route::post('logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
